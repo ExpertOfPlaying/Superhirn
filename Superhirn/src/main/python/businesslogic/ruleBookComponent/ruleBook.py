c2a6b3444cd3cb8181a72b3b0a98f708f439c996
Rico <rico.flemming@student.htw-berlin.de>
2023-12-20 19:01:42 +0100

    ruleBook inputchecks hälfte fertig

--- Superhirn/src/main/python/businesslogic/ruleBookComponent/ruleBook.py
+++ Superhirn/src/main/python/businesslogic/ruleBookComponent/ruleBook.py
@@ -1,36 +1,44 @@
 from validationError import ValidationError
 
 
-def check_stone_input(self, input_string):
-    stones = [int(num) for num in input_string]
-    for stone in stones:
-        if stone < 2 or stone > 8:
-            raise ValidationError("Input must be between 2 and 8")
-        else:
+def check_stone_input(input_string, min_number, max_number):
+    if input_string.isdigit():
+        stones = [int(num) for num in input_string]
+        for stone in stones:
+            if stone < min_number or stone > max_number:
+                raise ValidationError("Input must be integers between 2 and 8!")
+            else:
+                return True
+    else:
+        raise ValidationError("Input must be integers between 2 and 8!")
+
+
+def check_code_input(input_string, actual_board, max_number):  # coder gibt ein 12345 oder 1234
+    if len(input_string) == actual_board.code_max_length:
+        check_stone_input(input_string, 2, max_number)
+    else:
+        raise ValidationError(f"Input must be equal to {actual_board.code_max_length}!")
+
+
+def check_feedback_input(input_string, actual_board):
+    if len(input_string) <= actual_board.code_max_length or len(input_string) > 0:
+        if input_string == "":
             return True
+        else:
+            check_stone_input(input_string, 7, 8)
+    else:
+        raise ValidationError(f"Input must be equal or less to {actual_board.code_max_length}!")
 
 
-def check_input_int_only(self, input_string):
+def check_code_max_length_input():
     pass
 
 
-def check_code_input(self):
+def check_game_mode_input():
     pass
 
 
-def check_feedback_input(self):
-    pass
-
-
-def check_guessed_code_input(self):
-    pass
-
-
-def check_code_max_length_input(self):
-    pass
-
-
-def check_game_mode_input(self):
+def check_max_colour_input():
     pass
 
 
